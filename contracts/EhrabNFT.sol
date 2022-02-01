857e2216e930f19658f1d01df3d21acbb82b359c
Ali Swistak <aswistak@linkpoint.ca>
2022-02-01 14:13:20 -0800

    - Add scripts for deployment, add console.logs

--- contracts/EhrabNFT.sol
+++ contracts/EhrabNFT.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract EhrabNFT {
-	constructor () public {
-		console.log("This is my NFT contract. Whoa!");
+	constructor() {
+		console.log("Welcome to Ehrab's collection.");
 	}
 }
---
+++ scripts/run.js
@@ -0,0 +1,24 @@
+// No need to import ethers, can access it through Hardhat Runtime Environment, or 'HRE'
+
+const main = async () => {
+	// Compile our contract and generate the files needed to work with. Artifacts.
+	const nftContractFactory = await hre.ethers.getContractFactory('EhrabNFT');
+	// Deploy contract to local Ethereum network, new chain state every time, easier to debug errors
+	const nftContract = await nftContractFactory.deploy();
+	// Wait untill call to deploy() has resolved, constructor will run when contract is deployed
+	await nftContract.deployed();
+	// This will give us the address of the deployed contract
+	console.log("Contract deployed to:", nftContract.address);
+}
+
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+}
+
+runMain();
