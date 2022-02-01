3a62d0394356b126e09df493871229e46c5ea1bc
Ali Swistak <aswistak@linkpoint.ca>
2022-02-01 14:56:05 -0800

    - Inherit required ERC721 contracts. - Add mint function to main contract. - Add JSON metadata for NFT.

--- contracts/EhrabNFT.sol
+++ contracts/EhrabNFT.sol
@@ -1,11 +1,41 @@
 // SPDX-License-Identifier: UNLICENCED
-
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract EhrabNFT {
-	constructor() {
+
+// Inherit imported contract. Will have access to that contract's methods
+contract EhrabNFT is ERC721URIStorage {
+	// Utility from OpenZeppelin to keep track of tokenIds.
+	using Counters for Counters.Counter;
+	// Initialize contract state in contract storage
+	Counters.Counter private _tokenIds;
+	
+	// We need to set the name of our NFTs token and its symbol
+	constructor() ERC721 ("SquareNFT", "SQUARE") {
 		console.log("Welcome to Ehrab's collection.");
 	}
+
+	// Function for user to hit to get theri NFT
+	function mintEhrabNFT() public {
+		// Get the current tokenId, this starts at 0.
+		uint256 newItemId = _tokenIds.current();
+
+		// Actually mint the NFT to the sender using msg.sender passing tokenId as argument
+		_safeMint(msg.sender, newItemId);
+
+		// Set the NFTs data. Pass tokenId, and NFT metadata in JSON format, link to resource.
+		_setTokenURI(newItemId, "https://jsonkeeper.com/b/WJWZ");
+		console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+		// Increment the counter for when the next NFT is minted.
+		_tokenIds.increment();
+	}
+
+
+
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,14 +2,24 @@
 
 const main = async () => {
 	// Compile our contract and generate the files needed to work with. Artifacts.
-	const nftContractFactory = await hre.ethers.getContractFactory('EhrabNFT');
+	const nftContractFactory = await hre.ethers.getContractFactory("EhrabNFT");
 	// Deploy contract to local Ethereum network, new chain state every time, easier to debug errors
 	const nftContract = await nftContractFactory.deploy();
 	// Wait untill call to deploy() has resolved, constructor will run when contract is deployed
 	await nftContract.deployed();
 	// This will give us the address of the deployed contract
 	console.log("Contract deployed to:", nftContract.address);
-}
+
+	// Call the function.
+	let tx = await nftContract.mintEhrabNFT();
+	// Wait for it to be mined.
+	await tx.wait();
+
+	// Mint another NFT for fun.
+	tx = await nftContract.mintEhrabNFT();
+	// Wait for it to be mined.
+	await tx.wait();
+};
 
 const runMain = async () => {
 	try {
@@ -19,6 +29,6 @@ const runMain = async () => {
 		console.log(error);
 		process.exit(1);
 	}
-}
+};
 
 runMain();
