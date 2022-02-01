ef96ca4fb75710ecd5c2b0fe12c2eb76fcf54e6a
Ali Swistak <aswistak@linkpoint.ca>
2022-02-01 12:26:53 -0800

    - First commit.

---
+++ contracts/EhrabNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENCED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract EhrabNFT {
+	constructor () public {
+		console.log("This is my NFT contract. Whoa!");
+	}
+}
